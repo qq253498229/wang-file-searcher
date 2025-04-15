0712ee3e2000392adc807c4ca8cb2f82ee7cdca2
王斌 <253498229@qq.com>
2025-04-15 15:42:35 +0800

    调整标题

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from './shared/shared.module';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, SharedModule, NzMenuModule, RouterLink, RouterOutlet],
+  imports: [CommonModule, SharedModule, NzMenuModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  isCollapsed = false;
 }
