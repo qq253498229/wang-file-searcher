4e7a0811160491e26bb4dc0efc0893507c27bbe6
大胃王 <253498229@qq.com>
2025-05-15 16:50:00 +0800

    调整文件结构

--- src-tauri/src/command/search.rs
+++ src-tauri/src/command/search.rs
@@ -1,9 +1,7 @@
+use crate::utils::file_utils::merge_path;
 use serde::{Deserialize, Serialize};
 use std::fs;
-use std::fs::File;
-use std::io::Read;
-use std::path::{Path, PathBuf};
-use crate::utils::file_utils::is_binary_file;
+use std::path::PathBuf;
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct SearchOptions {
@@ -31,8 +29,6 @@ fn search_files(options: SearchOptions) -> anyhow::Result<()> {
     let search_text = options.text.as_ref().unwrap();
     let all_file_path = find_all_path(&options)?;
     for path in all_file_path.iter() {
-        let is_binary = is_binary_file(path)?;
-        println!("1");
         let content = fs::read(path)?;
     }
     // fs::metadata()
@@ -50,47 +46,6 @@ fn find_all_path(options: &SearchOptions) -> anyhow::Result<Vec<PathBuf>> {
     }
     Ok(path_list)
 }
-/// 合并路径，忽略子目录，只保留最上层的父目录
-fn merge_path(list: &mut Vec<PathBuf>, new_path: PathBuf) -> anyhow::Result<()> {
-    let mut has_child = false;
-    // 检查路径关系
-    for old_path in list.iter() {
-        if old_path == &new_path || new_path.starts_with(old_path) {
-            break;
-        }
-        if old_path.starts_with(&new_path) {
-            has_child = true;
-            break;
-        }
-    }
-    if has_child {
-        // 删除所有子路径并添加新父路径
-        list.retain(|old_path| !old_path.starts_with(&new_path));
-        list.push(new_path);
-    } else {
-        list.push(new_path);
-    }
-    Ok(())
-}
-
-fn read_dir_files(path: &Path) -> anyhow::Result<Vec<PathBuf>> {
-    let mut files = vec![];
-    let path = Path::new(path);
-    if path.is_dir() {
-        for entry in fs::read_dir(&path)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                files.append(&mut read_dir_files(&path)?);
-            } else if path.is_file() {
-                files.push(path);
-            }
-        }
-    } else if path.is_file() {
-        files.push(path.to_path_buf());
-    }
-    Ok(files)
-}
 
 #[cfg(test)]
 mod tests {
@@ -106,9 +61,8 @@ mod tests {
         let path = fs::canonicalize("tests/txt/sub-folder-1-1")?;
         let path = path.to_str().unwrap().to_string();
         path_list.push(path);
-        println!("path={:#?}", &path_list);
         options.path = Some(path_list);
-        let result = search_files(options);
+        search_files(options)?;
         Ok(())
     }
 }
--- src-tauri/src/utils/file_utils.rs
+++ src-tauri/src/utils/file_utils.rs
@@ -1,21 +1,7 @@
+use std::fs;
 use std::fs::File;
 use std::io::Read;
-use std::path::Path;
-
-/// 判断文件是否是二进制文件
-pub fn is_binary_file<P: AsRef<Path>>(path: P) -> std::io::Result<bool> {
-    let mut file = File::open(path)?;
-    let mut buffer = [0u8; 1024]; // 只读取前 1024 字节
-    let bytes_read = file.read(&mut buffer)?;
-
-    // 检查读取的字节中是否包含非打印字符
-    for &byte in &buffer[..bytes_read] {
-        if byte < 32 && byte != 10 && byte != 13 {
-            return Ok(true); // 发现非打印字符，认为是二进制文件
-        }
-    }
-    Ok(false) // 未发现非打印字符，认为不是二进制文件
-}
+use std::path::{Path, PathBuf};
 
 /// 查找文件内容是否包含指定字符串
 /// 使用字符级匹配算法，逐个字符读取文件内容
@@ -46,14 +32,64 @@ pub fn find_string_in_file<P: AsRef<Path>>(path: P, target: &str) -> std::io::Re
     Ok(false) // 未匹配到
 }
 
+/// 读取目录内的全部文件
+pub fn read_dir_files(path: &PathBuf) -> anyhow::Result<Vec<PathBuf>> {
+    let mut files = vec![];
+    if path.is_dir() {
+        for entry in fs::read_dir(&path)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                files.append(&mut read_dir_files(&path)?);
+            } else if path.is_file() {
+                files.push(path);
+            }
+        }
+    } else if path.is_file() {
+        files.push(path.to_path_buf());
+    }
+    Ok(files)
+}
+/// 合并路径，忽略子目录，只保留最上层的父目录
+pub fn merge_path(list: &mut Vec<PathBuf>, new_path: PathBuf) -> anyhow::Result<()> {
+    let mut has_child = false;
+    // 检查路径关系
+    for old_path in list.iter() {
+        if old_path == &new_path || new_path.starts_with(old_path) {
+            break;
+        }
+        if old_path.starts_with(&new_path) {
+            has_child = true;
+            break;
+        }
+    }
+    if has_child {
+        // 删除所有子路径并添加新父路径
+        list.retain(|old_path| !old_path.starts_with(&new_path));
+        list.push(new_path);
+    } else {
+        list.push(new_path);
+    }
+    Ok(())
+}
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test_find_string_in_file() -> anyhow::Result<()> {
         assert!(!find_string_in_file("tests/txt/test1.txt", "Hell1")?);
         assert!(find_string_in_file("tests/txt/test1.txt", "hello")?);
         assert!(find_string_in_file("tests/txt/test1.txt", "HelLo")?);
+        Ok(())
+    }
+    #[test]
+    fn test_merge_path() -> anyhow::Result<()> {
+        let mut list = vec![];
+        merge_path(&mut list, PathBuf::from("tests/txt/sub-folder-1-1"))?;
+        merge_path(&mut list, PathBuf::from("tests/txt"))?;
+        assert_eq!(1, list.len());
+        assert_eq!("tests/txt", list[0].to_str().unwrap());
         Ok(())
     }
 }
