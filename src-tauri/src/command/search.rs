478a8450b6a0d967544b47b4ec8dd9fa07e22e71
大胃王 <253498229@qq.com>
2025-05-15 17:28:23 +0800

    完成搜索流程

--- src-tauri/src/command/search.rs
+++ src-tauri/src/command/search.rs
@@ -1,6 +1,5 @@
-use crate::utils::file_utils::merge_path;
+use crate::utils::file_utils::{find_string_in_file, merge_path, read_dir_files};
 use serde::{Deserialize, Serialize};
-use std::fs;
 use std::path::PathBuf;
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -17,22 +16,25 @@ impl SearchOptions {
     }
 }
 #[tauri::command]
-pub fn search(options: SearchOptions) -> Result<String, ()> {
-    println!("search options: {:?}", options);
-    Ok("search".to_string())
+pub fn search(options: SearchOptions) -> Result<Vec<PathBuf>, String> {
+    let result = search_files(&options).map_err(|e| e.to_string());
+    result
 }
 
 /// 根据选项搜索全部文件
-fn search_files(options: SearchOptions) -> anyhow::Result<()> {
-    println!("search_files options: {:?}", options);
-    // fs::read("tests/txt/")
+fn search_files(options: &SearchOptions) -> anyhow::Result<Vec<PathBuf>> {
     let search_text = options.text.as_ref().unwrap();
     let all_file_path = find_all_path(&options)?;
-    for path in all_file_path.iter() {
-        let content = fs::read(path)?;
+    let mut r = vec![];
+    for path in all_file_path.into_iter() {
+        let files = read_dir_files(&path)?;
+        for p in files.into_iter() {
+            if find_string_in_file(&p, search_text)? {
+                r.push(p);
+            }
+        }
     }
-    // fs::metadata()
-    Ok(())
+    Ok(r)
 }
 /// 获取所有的搜索路径
 fn find_all_path(options: &SearchOptions) -> anyhow::Result<Vec<PathBuf>> {
@@ -50,6 +52,7 @@ fn find_all_path(options: &SearchOptions) -> anyhow::Result<Vec<PathBuf>> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs;
     #[test]
     fn test_search() -> anyhow::Result<()> {
         let mut options = SearchOptions::new();
@@ -62,7 +65,24 @@ mod tests {
         let path = path.to_str().unwrap().to_string();
         path_list.push(path);
         options.path = Some(path_list);
-        search_files(options)?;
+        let result = search_files(&options)?;
+        assert_eq!(5, result.len());
+        Ok(())
+    }
+    #[test]
+    fn test_search1() -> anyhow::Result<()> {
+        let mut options = SearchOptions::new();
+        options.text = Some("test2".to_string());
+        let mut path_list = vec![];
+        let path = fs::canonicalize("tests/txt")?;
+        let path = path.to_str().unwrap().to_string();
+        path_list.push(path);
+        let path = fs::canonicalize("tests/txt/sub-folder-1-1")?;
+        let path = path.to_str().unwrap().to_string();
+        path_list.push(path);
+        options.path = Some(path_list);
+        let result = search_files(&options)?;
+        assert_eq!(1, result.len());
         Ok(())
     }
 }
--- src-tauri/src/utils/file_utils.rs
+++ src-tauri/src/utils/file_utils.rs
@@ -3,20 +3,35 @@ use std::fs::File;
 use std::io::Read;
 use std::path::{Path, PathBuf};
 
+/// 判断文件是否是二进制文件
+pub fn is_binary_file<P: AsRef<Path>>(path: P) -> std::io::Result<bool> {
+    let mut file = File::open(path)?;
+    let mut buffer = [0u8; 1024]; // 只读取前 1024 字节
+    let bytes_read = file.read(&mut buffer)?;
+
+    // 检查读取的字节中是否包含非打印字符
+    for &byte in &buffer[..bytes_read] {
+        if byte < 32 && byte != 10 && byte != 13 {
+            return Ok(true); // 发现非打印字符，认为是二进制文件
+        }
+    }
+    Ok(false) // 未发现非打印字符，认为不是二进制文件
+}
 /// 查找文件内容是否包含指定字符串
 /// 使用字符级匹配算法，逐个字符读取文件内容
 pub fn find_string_in_file<P: AsRef<Path>>(path: P, target: &str) -> std::io::Result<bool> {
+    if is_binary_file(&path)? {
+        return Ok(false);
+    }
     let mut file = File::open(path)?;
     let mut buffer = [0u8; 1]; // 每次读取1个字节
     let target = target.as_bytes();
     let mut matched = 0;
-
     loop {
         let bytes_read = file.read(&mut buffer)?;
         if bytes_read == 0 {
             break; // 文件结束
         }
-
         let read_char = buffer[0].to_ascii_lowercase();
         let target_char = target[matched].to_ascii_lowercase();
         if read_char == target_char {
@@ -28,7 +43,6 @@ pub fn find_string_in_file<P: AsRef<Path>>(path: P, target: &str) -> std::io::Re
             matched = 0;
         }
     }
-
     Ok(false) // 未匹配到
 }
 
@@ -52,10 +66,12 @@ pub fn read_dir_files(path: &PathBuf) -> anyhow::Result<Vec<PathBuf>> {
 }
 /// 合并路径，忽略子目录，只保留最上层的父目录
 pub fn merge_path(list: &mut Vec<PathBuf>, new_path: PathBuf) -> anyhow::Result<()> {
+    let mut has_parent = false;
     let mut has_child = false;
     // 检查路径关系
     for old_path in list.iter() {
         if old_path == &new_path || new_path.starts_with(old_path) {
+            has_parent = true;
             break;
         }
         if old_path.starts_with(&new_path) {
@@ -63,7 +79,8 @@ pub fn merge_path(list: &mut Vec<PathBuf>, new_path: PathBuf) -> anyhow::Result<
             break;
         }
     }
-    if has_child {
+    if has_parent {
+    } else if has_child {
         // 删除所有子路径并添加新父路径
         list.retain(|old_path| !old_path.starts_with(&new_path));
         list.push(new_path);
