b0baa190f43c1628a1db386387ddb68b707425fd
王斌 <253498229@qq.com>
2025-05-17 01:33:08 +0800

    改成异步搜索，并向前端发送事件

--- src-tauri/src/command/search.rs
+++ src-tauri/src/command/search.rs
@@ -1,6 +1,10 @@
-use crate::utils::file_utils::{find_string_in_file, merge_path, read_dir_files};
+use crate::utils::file_utils::{find_string_in_file, merge_path};
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
+use std::{fs, thread};
+use tauri::{AppHandle, Emitter};
+use tracing::info;
+use walkdir::WalkDir;
 
 #[derive(Deserialize, Serialize, Debug, Default)]
 pub struct SearchOptions {
@@ -24,31 +28,68 @@ pub struct SearchPath {
     pub label: String,
     pub path: String,
 }
+#[derive(Deserialize, Serialize, Debug, Default, Clone)]
+pub struct SearchResult {
+    pub path: PathBuf,
+    pub size: u64,
+    pub create_at: u128,
+    pub update_at: u128,
+}
 #[tauri::command]
-pub fn search(options: SearchOptions) -> Result<Vec<PathBuf>, String> {
-    println!("options:{options:?}");
-    let result = search_files(&options).map_err(|e| {
-        eprintln!("{e:?}");
-        e.to_string()
+pub fn search(options: SearchOptions, app: AppHandle) -> Result<(), String> {
+    thread::spawn(move || {
+        let _ = search_files(&options, &app);
     });
-    result
+    Ok(())
 }
 
 /// 根据选项搜索全部文件
-fn search_files(options: &SearchOptions) -> anyhow::Result<Vec<PathBuf>> {
+fn search_files(options: &SearchOptions, app: &AppHandle) -> anyhow::Result<()> {
     let search_text = options.text.as_ref().unwrap();
     let all_file_path = find_all_path(&options)?;
-    println!("all_file_path:{all_file_path:?}");
-    let mut r = vec![];
-    for path in all_file_path.into_iter() {
-        let files = read_dir_files(path)?;
-        for p in files.into_iter() {
-            if find_string_in_file(&p, search_text)? {
-                r.push(p);
+    for path in all_file_path {
+        let _ = search_and_send_event(path, search_text, app);
+    }
+    Ok(())
+}
+/// 搜索文件，如果搜到了则向前端发送事件
+fn search_and_send_event(path: PathBuf, search_text: &str, app: &AppHandle) -> anyhow::Result<()> {
+    info!("search_and_send_event:{path:?}");
+    if path.is_dir() || !path.is_file() {
+        let walker = WalkDir::new(&path).into_iter();
+        for entry in walker.filter_map(|e| e.ok()) {
+            let new_path = entry.into_path();
+            if new_path == path {
+                continue;
             }
+            let _ = search_and_send_event(new_path, search_text, &app);
+        }
+        return Ok(());
+    }
+    if let Ok(r) = find_string_in_file(&path, search_text) {
+        if r {
+            let metadata = fs::metadata(&path);
+            let mut r = SearchResult {
+                path,
+                size: 0,
+                create_at: 0,
+                update_at: 0,
+            };
+            if let Ok(metadata) = metadata {
+                r.size = metadata.len();
+                r.update_at = metadata
+                    .modified()?
+                    .duration_since(std::time::SystemTime::UNIX_EPOCH)?
+                    .as_millis();
+                r.create_at = metadata
+                    .created()?
+                    .duration_since(std::time::SystemTime::UNIX_EPOCH)?
+                    .as_millis();
+            }
+            app.emit("search_result", r)?;
         }
     }
-    Ok(r)
+    Ok(())
 }
 /// 获取所有的搜索路径
 fn find_all_path(options: &SearchOptions) -> anyhow::Result<Vec<PathBuf>> {
@@ -63,6 +104,7 @@ fn find_all_path(options: &SearchOptions) -> anyhow::Result<Vec<PathBuf>> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::path::Path;
 
     #[test]
     fn test_search() -> anyhow::Result<()> {
@@ -70,7 +112,7 @@ mod tests {
         options.text = Some("test".to_string());
         options.add_includes("tests/txt".to_string());
         options.add_includes("tests/txt/sub-folder-1-1".to_string());
-        let result = search_files(&options)?;
+        let result = find_all_path(&options)?;
         assert_eq!(5, result.len());
         Ok(())
     }
@@ -80,7 +122,7 @@ mod tests {
         options.text = Some("test2".to_string());
         options.add_includes("tests/txt".to_string());
         options.add_includes("tests/txt/sub-folder-1-1".to_string());
-        let result = search_files(&options)?;
+        let result = find_all_path(&options)?;
         assert_eq!(1, result.len());
         Ok(())
     }
@@ -91,8 +133,26 @@ mod tests {
         options.text = Some("test2".to_string());
         options.add_includes("tests/txt".to_string());
         options.add_includes("tests/txt/sub-folder-1-1".to_string());
-        let result = search_files(&options)?;
+        let result = find_all_path(&options)?;
         assert_eq!(1, result.len());
         Ok(())
+    }
+
+    #[test]
+    fn test_file_path() {
+        let path = Path::new("1.txt");
+        assert_eq!("1", path.file_stem().unwrap().to_str().unwrap());
+        assert_eq!("txt", path.extension().unwrap().to_str().unwrap());
+        assert_eq!("1.txt", path.file_name().unwrap().to_str().unwrap());
+
+        let path = Path::new(".npmrc");
+        assert_eq!(".npmrc", path.file_stem().unwrap().to_str().unwrap());
+        assert_eq!(None, path.extension());
+        assert_eq!(".npmrc", path.file_name().unwrap().to_str().unwrap());
+
+        let path = Path::new("Dockerfile");
+        assert_eq!("Dockerfile", path.file_stem().unwrap().to_str().unwrap());
+        assert_eq!(None, path.extension());
+        assert_eq!("Dockerfile", path.file_name().unwrap().to_str().unwrap());
     }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { SharedModule } from './shared/shared.module';
 import { ContentInputGroupComponent } from './pages/content-input-group/content-input-group.component';
 import { LocationGroupComponent } from './pages/location-group/location-group.component';
 import { ResultGroupComponent } from './pages/result-group/result-group.component';
+import { listen } from '@tauri-apps/api/event';
 
 @Component({
   selector: 'app-root',
@@ -14,5 +15,8 @@ import { ResultGroupComponent } from './pages/result-group/result-group.componen
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
+    listen(`search_result`, e => {
+      console.log('callback===', e);
+    }).then();
   }
 }
