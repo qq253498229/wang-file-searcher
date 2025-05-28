ebe8e26e6110e3bb44592b640261427d798730b9
大胃王 <253498229@qq.com>
2025-05-28 16:09:09 +0800

    将【搜索到xx个文件】修改成【搜索到xx个结果】，避免文件和文件夹的误解

--- src/app/modules/result/result.selector.ts
+++ src/app/modules/result/result.selector.ts
@@ -23,7 +23,7 @@ export class ResultSelector {
           return '';
         }
         if (state.isStop) {
-          return `搜索完成，搜索到${state.result.length}个文件，总计耗时:${(state.statusTime - state.searchStartTime) / 1000}秒`;
+          return `搜索完成，搜索到${state.result.length}个结果，总计耗时:${(state.statusTime - state.searchStartTime) / 1000}秒`;
         } else {
           return `正在搜索:${state.statusPath}`;
         }
